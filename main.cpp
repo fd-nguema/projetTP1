01c7cec086b3b01c5a8bd04b11b3af46eace3ed1
fd-nguema <francoisdanefd@gmail.com>
2025-12-10 11:11:52 +0100

    Test de bon fonctionnement de la classe Book

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include "Date.h"
+#include "Book.h"
 
 int main() {
     try {
-        Date date1(4, 12, 2025);
-        std::cout << "Date1 : " << date1.date() << std::endl;
-        Date date2(32, 23, 1000);
-        std::cout << "Date2 : " << date2.date() << std::endl;
-        Date date3(15, 15, 2025);
-        std::cout << "Date3 : " << date3.date() << std::endl;
+        Date publication_date1(8, 6, 1949);
+        Book book1("1984", "George Orwell", "philosophique", publication_date1, "978-2-07-046369-5");
+        std::cout << "titre : " << book1.title() << std::endl <<
+                    "auteur : " << book1.author() << std::endl <<
+                    "genre : " << book1.genre() << std::endl << 
+                    "date de publication : " << book1.publicationDate() << std::endl <<
+                    "isbn : " << book1.isbn() << std::endl;
+        Book book2("", "George Orwell", "philosophique", publication_date1, "978-2-07-046369-5");
     } 
     catch (const std::invalid_argument& e) {
         std::cerr << "Erreur : " << e.what() << std::endl;
